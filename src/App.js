581544bb87950c1806521cd1a4fd57e038a7ff1d
satyamupwanshi <satyamupwanshi50@gmail.com>
2025-07-14 02:10:36 +0530

    dscsdcscs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
+import { BrowserRouter as Router , Routes , Route , Link} from 'react-router-dom'
 import About from './pages/About' ;
 import Home from './pages/Home' ;
+import Vans from './pages/Vans';
+import VanDetail from './pages/VanDetail';
 
 const App = () => {
 
@@ -9,10 +11,21 @@ const App = () => {
           <>
         
           <Router>
+            <header>
+          <Link className="site-logo" to="/">#VanLife</Link>
+          <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+
+          </nav>
+      </header>
             <Routes>
               <Route path="/" element={<Home/>}/>
               
               <Route path="/about" element={<About/>}/>
+              <Route path="/vans" element={<Vans />} />
+              <Route path="/vans/:id" element={<VanDetail />} />
+
             </Routes>
           </Router>
           </>
